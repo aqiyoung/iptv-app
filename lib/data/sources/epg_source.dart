00d9ac2cc908734ff07f1eee83ebd20e66b1428c
aqiyoung <aqiyoung.lee@gmail.com>
2026-06-16 08:53:09 +0800

    refactor(data): lib/data/sources/epg_source.dart (hand-written, no freezed)

--- lib/data/sources/epg_source.dart
+++ lib/data/sources/epg_source.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 
 import '../models/epg.dart';
 
+/// iptv-org EPG 数据源 (XMLTV, 巨大，卡 3 仅占位)
 class EpgSource {
   EpgSource({http.Client? client, this.endpoint = _defaultEndpoint})
       : _client = client ?? http.Client();
@@ -12,8 +13,8 @@ class EpgSource {
   final http.Client _client;
   final String endpoint;
 
+  /// 拉取某个国家/地区 EPG（卡 5 实现 XMLTV 解析）
   Future<List<EpgEntry>> fetchCountry(String cc) async {
-    // TODO(card 5): 实现 XMLTV 解析
     throw UnimplementedError('EPG parsing deferred to card 5');
   }
 
