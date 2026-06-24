69f28438c27598a098551f1e9ac7156f6660c0e1
threely <16693021+threely@user.noreply.gitee.com>
2026-06-24 08:27:55 +0800

    fix: 修 unnecessary_non_null_assertion warnings

--- lib/services/player_service.dart
+++ lib/services/player_service.dart
@@ -234,7 +234,7 @@ class PlayerService extends ChangeNotifier {
     //  stop/open 在 libmpv 内部可能交错执行 (stop 未完成就 open 新流).
     //  折中:  await stop (50-200ms),  但 set loading 在 stop 之前已经发出,
     //  UI 不会白屏.  stop 完成后立即 open,  不引入额外延迟.
-    await _player!.stop();
+    await _player.stop();
 
     try {
       final source = await _failover.play(
@@ -313,7 +313,7 @@ class PlayerService extends ChangeNotifier {
     }
 
     // 6/17 修声音残留: 跟 [play] 一样,  先 stop 旧 player 避免双声
-    await _player!.stop();
+    await _player.stop();
 
     _set(
       _state.copyWith(
